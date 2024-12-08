5abdc95f6e81f471821a09483fb02a7bbdda35b3
W11 <W11@MIKE>
2024-12-08 17:09:59 -0600

    utiliddades

---
+++ src/sistemavinculacion/modelo/Utilidades.java
@@ -0,0 +1,26 @@
+package sistemavinculacion.modelo;
+
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class Utilidades {
+        public static void mostrarAlertaSimple(String titulo, String mensaje, Alert.AlertType tipo){
+        Alert alerta = new Alert(tipo);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+    
+    public static boolean mostrarAlertaConfirmacion(String titulo, String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        Optional<ButtonType> respuesta = alerta.showAndWait();
+        return (respuesta.get()==ButtonType.OK);
+    }
+    
+    
+}
