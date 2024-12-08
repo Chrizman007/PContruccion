cdd4996b2000def5d53cb3e0ff9a21c28beee197
W11 <W11@MIKE>
2024-12-08 16:49:23 -0600

    conexion base de datos

--- src/sistemavinculacion/modelo/ConexionBD.java
+++ src/sistemavinculacion/modelo/ConexionBD.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemavinculacion.modelo;
 
-/**
- *
- * @author chris
- */
 public class ConexionBD {
-    
+    public static final String USER_BD = "Mike";
+    public static final String PASSWORD_BD = "Magc26%&";
 }
