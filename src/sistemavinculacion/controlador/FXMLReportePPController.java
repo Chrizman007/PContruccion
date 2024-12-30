2d26c3cadd3d7539721e76aa0eafc3590390254d
W11 <W11@MIKE>
2024-12-30 14:29:56 -0600

    vista

---
+++ src/sistemavinculacion/controlador/FXMLReportePPController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sistemavinculacion.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author W11
+ */
+public class FXMLReportePPController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
